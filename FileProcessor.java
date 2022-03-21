6af435bb1dba648ddfe8276494ed6af00a685ca7
C20384993 <C20384993@mytudublin.ie>
2022-03-21 13:13:26 +0000

    Changed use of Scanner to BufferedReader. All file lines are now read. Single word searches now match.

--- FileProcessor.java
+++ FileProcessor.java
@@ -1,7 +1,10 @@
 package com.oop.eng;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -40,6 +43,7 @@ public class FileProcessor {
 	//2.) If no filenames are entered, then all files will be searched for the search term(s).
 	//***NOTE: Will add way to check files in folder, rather than manually typing each filename in if statement. 
 	//
+	//Buffered Reader code: https://www.guru99.com/buffered-reader-in-java.html
 	public void compareString(String passedFileName, String searchTerms)
 	{
 		//Variables. These will be used in both options.
@@ -62,45 +66,73 @@ public class FileProcessor {
 			
 			//Integer for looping through the array.
 			int i = 0;
+			int y = 0;
 			
-			Scanner myScanner = null;
+			BufferedReader myBufferReader = null;
 			try {
-				myScanner = new Scanner(userFile);
-			} catch (FileNotFoundException e) {
+				myBufferReader = new BufferedReader(new FileReader(userFile));
+			} catch (FileNotFoundException e1) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				e1.printStackTrace();
 			}
 			
 			//Read through the file.
 			//
-			while(myScanner.hasNextLine())
-			{
-				//Debug Line
-				System.out.println("Inside while");
-				currentLine = myScanner.nextLine();
-				//roleLines[i] = currentLine
-				//Add the current line into the array.
-				textFileLines.add(currentLine);
-				//Debug line
-				System.out.println("this line is "+ currentLine);
-				System.out.println(textFileLines.get(i));
-				
-				//If current line has any words matching the searchTerms.
-				if((textFileLines.get(i)).equals(searchTerms))
+			try {
+				while((currentLine = myBufferReader.readLine()) != null)
 				{
+					//Dev note: The line being read means the whole line.
+					//A line is marked by \n, e.g. "Harris reviewed his earnings" vs "Harris\n reviewed\n" etc
+					//"Harris reviewed his earnings" is one line, but "Harris" "reviewed" are separate.
+					//So matching the searchTerm doesn't work for a line with more than 1 word in it.
+					//option 1.) Split each word in text file onto a separate line so it's 1 word per line.
+					//option 2.) Use some delimiter to identify each word in a line.
+					//option 3.) None of the above, use string.split on current line and use textFileLines.add
+					
+					//Debug Line
+					y=0;
+					System.out.println("Inside while");
+					//currentLine = myBufferReader.nextLine();
+					
+					//Split currentLine into an array that contains each word.
+					String[] separatedWords = currentLine.split(" ");
+					
+					//roleLines[i] = currentLine
+					
+					//Add the current line into the array.
+					//textFileLines.add(currentLine);
 					//Debug line
-					System.out.println("Search term matched.");
+					//System.out.println("this line is "+ currentLine);
+					//System.out.println(textFileLines.get(i));
 					
-					//Increase matched by 1.
-					numOfMatches++;
+					//Reminder: currentLine is split up into an array or strings, need to loop through array and 
+					//check which strings match, but how to know how many elements are in the array?
+					while(y<separatedWords.length)
+					{
+						System.out.println("word["+y+"] = "+separatedWords[y]);
+						//If current line has any words matching the searchTerms.
+						if((separatedWords[y]).equals(searchTerms))
+						{
+							//Debug line
+							System.out.println("Search term matched.");
+						
+							//Increase matched by 1.
+							numOfMatches++;
+						}
+						
+						y++;
+					}
+						
+					//Increment i.
+					i++;
+						
 				}
-					
-				//Increment i.
-				i++;
-					
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 			
-			myScanner.close();
+			//myScanner.close();
 			System.out.println(numOfMatches);
 		}
 		
